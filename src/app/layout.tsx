da8c1fbdd73caf7d2a8f47bb5b9491a7dc33a860
Davi Sobrinho <davisobrinho82452@gmail.com>
2025-07-14 09:33:19 -0300

    updated font usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,10 @@ import Navbar from "@/components/navBar";
 import Footer from "@/components/footer";
 
 const poppins = Poppins({
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    variable: "--font-poppins",
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    subsets: ['latin'],
 });
+
 
 export const metadata = {
     title: 'Portal de Notícias',
@@ -20,7 +21,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <body >
                 <div className="bg-slate-50 text-gray-900 suppressHydrationWarning">
                     <Navbar />
-                    <main className={`${poppins.variable} antialiased min-h-[100vh] px-4 py-8 container mx-auto `}>{children}
+                    <main className={`${poppins.className} antialiased min-h-[100vh] px-4 py-8 container mx-auto `}>{children}
                     </main>
                     <Footer />
                 </div>
