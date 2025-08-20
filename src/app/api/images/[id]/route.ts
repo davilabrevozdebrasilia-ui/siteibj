7859f850ffd15c5e12411563eac9e4c288cc468a
Davi Sobrinho <davisobrinho82452@gmail.com>
2025-08-20 09:32:56 -0300

    added page flipper and projects content, also a lot of changes to each page layout

--- src/app/api/images/[id]/route.ts
+++ src/app/api/images/[id]/route.ts
@@ -1,23 +1,23 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-interface Props {
-    params: { id: string };
-}
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const idStr = url.pathname.split("/").pop(); 
+  const id = Number(idStr);
 
-export async function GET({ params }: Props) {
-    const id = Number(params.id);
+  if (isNaN(id)) return new NextResponse("ID inválido", { status: 400 });
 
-    const imagem = await prisma.imagem.findUnique({
-        where: { id },
-        select: { url: true },
-    });
+  const imagem = await prisma.imagem.findUnique({
+    where: { id },
+    select: { url: true },
+  });
 
-    if (!imagem) return new NextResponse("Imagem não encontrada", { status: 404 });
+  if (!imagem) return new NextResponse("Imagem não encontrada", { status: 404 });
 
-    const buffer = Buffer.from(imagem.url.split(",")[1], "base64");
-    return new NextResponse(buffer, {
-        status: 200,
-        headers: { "Content-Type": "image/webp" },
-    });
+  const buffer = Buffer.from(imagem.url.split(",")[1], "base64");
+  return new NextResponse(buffer, {
+    status: 200,
+    headers: { "Content-Type": "image/webp" },
+  });
 }
