0cf0a3346e397e85acbee3f0ed92a4eddac755c4
Davi Sobrinho <davisobrinho82452@gmail.com>
2025-08-20 08:48:57 -0300

    added page flipper and projects content, also a lot of changes to each page layout

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,9 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-
+import sharp from "sharp";
 
 export async function POST(req: NextRequest) {
-
     const data = await req.json();
 
     if (!data.titulo || !data.url) {
@@ -21,11 +20,20 @@ export async function POST(req: NextRequest) {
     }
 
     try {
+        const response = await fetch(data.url);
+        const buffer = Buffer.from(await response.arrayBuffer());
+
+        const optimizedBuffer = await sharp(buffer)
+            .webp({ quality: 75 })
+            .toBuffer();
+
+        const optimizedBase64 = `data:image/webp;base64,${optimizedBuffer.toString("base64")}`;
+
         const imagemCriada = await prisma.imagem.create({
             data: {
                 titulo: data.titulo,
                 descricao: data.descricao || null,
-                url: data.url,
+                url: optimizedBase64,
                 projetos: data.projetos
             },
         });
