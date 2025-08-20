720a6a08c7f7962bfdaf69ce312896030e357364
Davi Sobrinho <davisobrinho82452@gmail.com>
2025-08-20 09:40:45 -0300

    added page flipper and projects content, also a lot of changes to each page layout

--- src/app/api/images/[id]/route.ts
+++ src/app/api/images/[id]/route.ts
@@ -1,28 +1,23 @@
 import { prisma } from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(
-    req: NextRequest,
-    { params }: { params: { id: string } }
-) {
-    const id = Number(params.id);
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const idStr = url.pathname.split("/").pop(); 
+  const id = Number(idStr);
 
-    try {
-        const imagem = await prisma.imagem.findUnique({
-            where: { id },
-            select: { url: true },
-        });
+  if (isNaN(id)) return new NextResponse("ID inválido", { status: 400 });
 
-        if (!imagem) return new NextResponse("Imagem não encontrada", { status: 404 });
+  const imagem = await prisma.imagem.findUnique({
+    where: { id },
+    select: { url: true },
+  });
 
-        const buffer = Buffer.from(imagem.url.split(",")[1], "base64");
+  if (!imagem) return new NextResponse("Imagem não encontrada", { status: 404 });
 
-        return new NextResponse(buffer, {
-            status: 200,
-            headers: { "Content-Type": "image/webp" },
-        });
-    } catch (error) {
-        console.error("Erro ao buscar imagem:", error);
-        return new NextResponse("Erro interno", { status: 500 });
-    }
+  const buffer = Buffer.from(imagem.url.split(",")[1], "base64");
+  return new NextResponse(buffer, {
+    status: 200,
+    headers: { "Content-Type": "image/webp" },
+  });
 }
