343f23cb945cb41baea255c89c4efa0f32662333
Davi Sobrinho <davisobrinho82452@gmail.com>
2025-08-27 11:42:00 -0300

    Last version

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -66,23 +66,28 @@ export default function Navbar() {
 
     return (
         <header className="sticky top-0 z-50 bg-white border-b">
-            <div className="flex justify-between items-center px-4 py-2 z-50">
-                <Link href="/" className="flex items-center gap-2">
-                    <img
-                        src="/logo.jpg"
-                        alt="Instituto Brazil Just"
-                        className="h-full w-auto max-w-[80px] object-contain"
-                    />
-                    <span className="text-xl font-bold text-blue-900">
-                        Instituto Brazil Just
-                    </span>
-                </Link>
+            <Link
+                href="/"
+                className="relative flex items-center w-full h-32 md:h-40 lg:h-48 rounded-xl shadow-md overflow-hidden bg-cover bg-center"
+                style={{ backgroundImage: "url('/banner6.png')" }}
+            >
+                <div
+                    className="h-full w-full bg-contain bg-left bg-no-repeat ms-4"
+                    style={{ backgroundImage: "url('/banner5.png')" }}
+                />
 
-                <div className="sm:text-lg text-md text-blue-900 font-mono bg-slate-100 px-2 py-1 rounded shadow-sm font-bold flex items-center gap-2">
-                    <Calendar className="w-4 h-4 text-blue-900" />
-                    {horaAtual}
+                <div className="flex-1 h-full flex justify-end items-center">
+                    <div className="flex items-center gap-2 px-4 py-2 bg-slate-100/90 rounded-lg shadow-md mr-4">
+                        <Calendar className="w-4 h-4 text-blue-900" />
+                        <span className="sm:text-lg text-md text-blue-900 font-mono font-bold">
+                            {horaAtual}
+                        </span>
+                    </div>
                 </div>
-            </div>
+            </Link>
+
+
+
 
             <div className="h-12 flex items-center lg:justify-center justify-between px-4 bg-blue-500 drop-shadow-slate-400 drop-shadow-md ">
                 <nav className="hidden lg:flex gap-4 flex-wrap justify-end relative">
@@ -118,8 +123,8 @@ export default function Navbar() {
                                 key={link.href}
                                 href={link.href}
                                 className={`text-md px-2 py-3 whitespace-nowrap h-full hover:bg-blue-400 transition ${pathname === link.href
-                                        ? "text-slate-100 font-bold"
-                                        : "text-slate-100"
+                                    ? "text-slate-100 font-bold"
+                                    : "text-slate-100"
                                     }`}
                             >
                                 {link.label}
@@ -176,8 +181,8 @@ export default function Navbar() {
                                             key={link.href}
                                             href={link.href}
                                             className={`text-base font-medium py-1 px-2 rounded hover:underline ${pathname === link.href
-                                                    ? "text-blue-900 font-bold"
-                                                    : "text-blue-700"
+                                                ? "text-blue-900 font-bold"
+                                                : "text-blue-700"
                                                 }`}
                                         >
                                             {link.label}
